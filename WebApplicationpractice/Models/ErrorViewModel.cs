4f7443a17c77c0b7eb3f42c727f8072ce90b3072
Narendra <narendrakotika3576@gmail.com>
2024-02-27 16:01:07 +0530

    Add project files.

---
+++ WebApplicationpractice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationpractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
